526ac4da512bc06e41398f336320a2c4edf83b7f
Kanta Shanai <zeromindful@gmail.com>
2020-06-11 13:03:38 +0900

    herokuのauto_incrementの仕様に対応

--- database/seeds/Const_ordersTableSeeder.php
+++ database/seeds/Const_ordersTableSeeder.php
@@ -16,10 +16,10 @@ class Const_ordersTableSeeder extends Seeder
         DB::insert('INSERT INTO const_orders (const_name, place, genre, user_id, status)
             VALUES ("A処理場ポンプ電気設備工事", "A処理場", "電気", 1, "設計中")');
         DB::insert('INSERT INTO const_orders (const_name, place, genre, user_id, status)
-            VALUES ("B処理場送風機設備工事", "B処理場", "機械", 2, "設計中")');
+            VALUES ("B処理場送風機設備工事", "B処理場", "機械", 11, "設計中")');
         DB::insert('INSERT INTO const_orders (const_name, place, genre, user_id, status, order_date)
-            VALUES ("C処理場送風機設備工事", "A処理場", "機械", 2, "発注済", "2020-5-1")');
+            VALUES ("C処理場送風機設備工事", "A処理場", "機械", 11, "発注済", "2020-5-1")');
         DB::insert('INSERT INTO const_orders (const_name, place, genre, user_id, status)
-            VALUES ("C処理場送風機電気設備工事", "A処理場", "電気", 3, "設計中")');
+            VALUES ("C処理場送風機電気設備工事", "A処理場", "電気", 21, "設計中")');
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,8 +11,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::insert('INSERT INTO users (name, email, password) VALUES ("テストユーザー", "test@example.com", "test0000")');
-        DB::insert('INSERT INTO users (name, email, password) VALUES ("鈴木太郎", "tarou@example.com", "00000000")');
-        DB::insert('INSERT INTO users (name, email, password) VALUES ("山田花子", "hanako@example.com", "00000000")');
+        DB::table('users')->insert([
+            'id' => '1',
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => bcrypt('test0000'),
+        ]);
+        DB::table('users')->insert([
+            'id' => '11',
+            'name' => '鈴木太郎',
+            'email' => 'tarou@example.com',
+            'password' => bcrypt('00000000'),
+        ]);
+        DB::table('users')->insert([
+            'id' => '21',
+            'name' => '山田花子',
+            'email' => 'hanako@example.com',
+            'password' => bcrypt('00000000'),
+        ]);
+
+
+
+        // DB::insert('INSERT INTO users (name, email, password) VALUES ("テストユーザー", "test@example.com", "test0000")');
+        // DB::insert('INSERT INTO users (name, email, password) VALUES ("鈴木太郎", "tarou@example.com", "00000000")');
+        // DB::insert('INSERT INTO users (name, email, password) VALUES ("山田花子", "hanako@example.com", "00000000")');
     }
 }
